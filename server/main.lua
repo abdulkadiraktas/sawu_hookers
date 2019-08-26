07167b4cc898cc2a36d61a0051a56da9592518a4
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2019-08-26 23:20:38 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -30,17 +30,34 @@ AddEventHandler('sawu_hookers:pay', function(boolean)
         if xPlayer.getMoney() >= Config.BlowjobPrice then
             xPlayer.removeMoney(Config.BlowjobPrice)
             TriggerClientEvent('sawu_hookers:startBlowjob', _source)
-            TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Bu güzel hizmet için ' .. Config.BlowjobPrice ..' $ para ödedin.' })
+            TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'You payed: ' .. Config.BlowjobPrice ..' $' })
+            -- this adds money to society_nightclub
+            if Config.SocietyNightclub then
+                TriggerEvent('esx_addonaccount:getSharedAccount', 'society_nightclub', function(account)
+                    account.addMoney(Config.BlowjobPrice)
+                end)
+            end
         else
-            TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'error', text = 'Yeterli paraya sahip değilsin!' })
+            TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'error', text = 'You do not have enough money' })
+            TriggerClientEvent('sawu_hookers:noMoney', _source)
         end  
     else
         if xPlayer.getMoney() >= Config.SexPrice then
             xPlayer.removeMoney(Config.SexPrice)
             TriggerClientEvent('sawu_hookers:startSex', _source)
-            TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'Bu güzel hizmet için ' .. Config.SexPrice ..' $ para ödedin.' })
+            TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'inform', text = 'You payed: ' .. Config.SexPrice ..' $' })
+            -- this adds money to society_nightclub
+            if Config.SocietyNightclub then
+                TriggerEvent('esx_addonaccount:getSharedAccount', 'society_nightclub', function(account)
+                    account.addMoney(Config.SexPrice)
+                end)
+            end
         else
-            TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'error', text = 'Yeterli paraya sahip değilsin!' })
+            TriggerClientEvent('mythic_notify:client:SendAlert', _source, { type = 'error', text = 'You do not have enough money' })
+            TriggerClientEvent('sawu_hookers:noMoney', _source)
         end 
     end
 end)
+
+
+
